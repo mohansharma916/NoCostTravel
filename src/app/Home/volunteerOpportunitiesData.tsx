63f2459ca50fde490ceb397800bca6cc2153af65
Owais <owais.it@ramaera.in>
2023-12-30 10:07:43 +0530

    add

--- src/app/Home/volunteerOpportunitiesData.tsx
+++ src/app/Home/volunteerOpportunitiesData.tsx
@@ -2,20 +2,22 @@ import React from "react";
 
 const volunteerOpportunitiesData = [
   {
-    location: "Maharashtra",
+    location: "Himachal Pradesh",
     need: "Needs Help Urgently",
     icon: "/images/hero-1.jpeg",
-    title: "Title 1",
-    detail: "Details 1",
-    tags: ["Tag1", "Tag2"],
+    title: "Volunteer In Kullu",
+    detail:
+      "Welcome to the our Resort, where luxury meets nature in the heart of the mountains. Our carefully designed... ",
+    tags: ["Videographer", "Photographer", "+ More"],
   },
   {
-    location: "Tamil Nadu",
+    location: "Himachal Pradesh",
     need: "Needs Help Urgently",
     icon: "/images/hero-2.jpeg",
-    title: "Title 1",
-    detail: "Details 1",
-    tags: ["Tag1", "Tag2"],
+    title: "Volunteer In Manali",
+    detail:
+      "Our property is located within the hearts of mountain. Manali is a high-altitude Himalayan resort town in... ",
+    tags: ["Musician", "Videographer", "+ More"],
   },
   {
     location: "Delhi",
