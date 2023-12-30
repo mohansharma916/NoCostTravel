1803397a44a86d1575859bcb87d887de5d362f6b
Owais <owais.it@ramaera.in>
2023-12-30 18:07:30 +0530

    RecentVolunteers Slider

--- src/app/home/RecentVolunteers.tsx
+++ src/app/home/RecentVolunteers.tsx
@@ -1,19 +1,89 @@
 "use client";
-import React from "react";
-import SimpleSlider from "./SimpleSlider";
+import { useEffect, useRef, useState } from "react";
+import Slider from "react-slick";
+import Image from "next/image";
+import { MapPinIcon } from "@heroicons/react/24/outline";
+
+const blogs = [
+  {
+    image: "/images/airplane.png",
+    name: "Deepak",
+    location: "Himachal Pradesh",
+    message:
+      "Hi , I'm a self taught illustrator, graphic designer and an architect by profession.currently working on my 20s to make.",
+    tags: ["Musician", "Videographer", "Community Engagement", "+ More"],
+  },
+  {
+    image: "/images/hotel.png",
+    name: "Blog 1222",
+    location: "Delhi",
+    message:
+      "Hi , I'm a self taught illustrator, graphic designer and an architect by profession.currently working on my 20s to make.",
+    tags: ["Musician", "Videographer", "Community Engagement", "+ More"],
+  },
+  {
+    image: "/images/luggage.png",
+    name: "ssdds 1222",
+    location: "Uttar Pradesh",
+    message:
+      "Hi , I'm a self taught illustrator, graphic designer and an architect by profession.currently working on my 20s to make.",
+    tags: ["Musician", "Videographer", "Community Engagement", "+ More"],
+  },
+];
 
 const RecentVolunteers = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
-    <>
-      <div className="flex justify-center pb-10">
-        <div className="bg-[url('/images/blog/volunteerbg.png')] bg-cover  w-[95%] h-[620px] rounded-2xl flex justify-center">
-          <div className="text-white text-4xl	font-semibold flex flex-col items-center	  mt-2">
-            <p>Recent Volunteers</p>
-            <SimpleSlider />
-          </div>
-        </div>
+    <div className="relative w-full bg-[url('/images/blog/volunteerbg.png')] bg-cover bg-center p-4">
+      <h1 className="text-center text-white text-4xl font-bold">
+        Recent Volunteers
+      </h1>
+      <div className=" mx-auto p-4 ">
+        <Slider {...settings}>
+          {blogs.map((blog, index) => (
+            <div key={index}>
+              <div className="flex flex-col items-center justify-center mx-auto">
+                <div className="rounded-full overflow-hidden h-32 w-32 border border-slate-300 mb-8">
+                  <Image
+                    src={blog.image}
+                    alt={blog.name}
+                    width={300}
+                    height={300}
+                  />
+                </div>
+
+                <div className="bg-slate-300 text-white bg-opacity-40 backdrop-blur-sm  rounded-lg p-2 w-full sm:w-80 border-[0.5px] border-slate-400">
+                  <div className="flex items-center">
+                    <MapPinIcon className=" w-6" />
+                    <p className="ml-2 text-sm">{blog.location}</p>
+                  </div>
+                  <h2 className="font-semibold mt-2">{blog.name}</h2>
+
+                  <p className="text-sm my-2">{blog.message}</p>
+                  <div className="flex mt-2  overflow-x-auto w-full whitespace-nowrap">
+                    {blog.tags.map((tag, idx) => (
+                      <div
+                        key={idx}
+                        className="w-full text-white text-xs  px-2 py-1 m-1 bg-opacity-50 bg-slate-300 backdrop-blur-sm backdrop-filter rounded-md border">
+                        {tag}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </Slider>
       </div>
-    </>
+    </div>
   );
 };
+
 export default RecentVolunteers;
